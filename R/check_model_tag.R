e30c9eaf386f73f0cf38c4693112f14b9a641906
Theo Pannetier <theo.pann@gmail.com>
2018-09-14 16:40:02 +0200

    - fixed DDVTD_tags filename call to DDvTD_tags - rm(res) only if res exists

--- R/check_model_tag.R
+++ R/check_model_tag.R
@@ -1,5 +1,5 @@
 check_model_tag <- function(model_tag){
-  data("./data/DDVTD_tags.RData")
+  data("./data/DDvTD_tags.RData")
   if( !(model_tag %in% DDvTD_tags) ){
     stop("Incorrect model specified - see data(DDvTD_tags) for accepted model inputs.")
   }
--- R/optimize_model.R
+++ R/optimize_model.R
@@ -58,7 +58,7 @@ optimize_model <- function(sim_model, para, optim_model, init = 1,
   # Default rangemc extend to all trees in the input dataset
   if( is.null(rangemc) ){ rangemc <- seq_along(trees) }
 
-  rm(res)
+  if(exists(res)) { rm(res) }
   if(overwrite){
     cat("Previous results will be overwritten.\n")
     } else {
@@ -118,7 +118,7 @@ optimize_model <- function(sim_model, para, optim_model, init = 1,
   }
 
   # Tidy dataset format
-  data("./data/DDVTD_tags.RData")
+  data("./data/DDvTD_tags.RData")
   res <- data.frame(
     "sim" = factor(sim_model, levels = DDvTD_tags),
     "true_lambda0" = pars[2],
