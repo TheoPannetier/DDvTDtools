0bcb4903de785f0c439394bf536caacdcf89857f
Theo Pannetier <theo.pann@gmail.com>
2018-09-14 16:34:07 +0200

    Corrected argument name in check_model_tag.

--- R/check_model_tag.R
+++ R/check_model_tag.R
@@ -1,6 +1,6 @@
 check_model_tag <- function(model_tag){
   data("./data/DDVTD_tags.RData")
-  if( !(model %in% DDvTD_tags) ){
+  if( !(model_tag %in% DDvTD_tags) ){
     stop("Incorrect model specified - see data(DDvTD_tags) for accepted model inputs.")
   }
 
