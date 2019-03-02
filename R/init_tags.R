65fd31d9b4f8e180d4c05c5ffeec296c729e4193
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-03-02 14:27:10 +0100

    removed optimize_model and dependencies

--- R/init_tags.R
+++
@@ -1,9 +0,0 @@
-#' Initial conditions currently accepted in DDvTDtools
-#'
-#' A vector containing the currently accepted initial conditions in DDvTDtools.
-#'
-#' @usage data("init_tags")
-#'
-#' @format A numerical vector which elements are a single-digit numerical value.
-#'
-"init_tags"
