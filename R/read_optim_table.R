0f80a4407c08ce9ed0d8beb2b22cdc5f4ce8eb99
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-03-17 23:46:15 +0100

    add get_dAIC_DDvTD

--- R/read_optim_table.R
+++ R/read_optim_table.R
@@ -4,22 +4,18 @@
 #'
 #' @param sim name of the model used to simulate the trees. See \code{get_sim_names()}
 #' @param optim name of the model fitted to the simulated trees. See \code{get_optim_names()}
-#' @param init integer, the initialization parameter of model fitting.
 #' @param para four-digit-character specifying the parameters used to simulate the trees. See \code{get_para_values}.
 #'
 #' @author Theo Pannetier
 #' @export
 
-read_optim_table <- function(sim, optim, init, para){
+read_optim_table <- function(sim, optim, para){
   assert_DDvTD_wd()
   assert_sim(sim)
   assert_para(para)
   assert_optim(optim)
-  if(!is.numeric(init)){
-    stop("init should be a numeric")
-  }
 
-  res_filename <- paste0("sim", sim, "_optim", optim, "_init", init, "-", para, ".rds")
+  res_filename <- paste0("sim", sim, "_optim", optim, "-", para, ".rds")
 
   if(!file.exists(paste0("data/optim/", res_filename))){
     stop(paste0("data/optim/",res_filename, " does not exist"))
