04624d0d20e8472d7d6d21befc5b4aff915392ca
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-03-17 15:59:39 +0100

    add get_Kprime

---
+++ R/get_Kprime.R
@@ -0,0 +1,12 @@
+#' Compute K' from la0, mu0, K
+#'
+#' @param pars a numeric vector containing c(la0, mu0, K)
+#' 
+#' @author Théo Pannetier
+#' @export
+#' 
+get_Kprime <- function(pars){
+  if(!is.numeric(pars)|length(pars) != 3){stop("Invalid input. Please check pars argument")}
+  pars[1] * pars[3] / (pars[1] - pars[2])
+  # Kprime = la0 * K / (la0 - mu0)
+}
