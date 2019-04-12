251034895e071f1158b295ffd3ef0d09689942fa
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-04-12 13:49:23 +0200

    fixed indentation in documentation

--- R/get_brts.R
+++ R/get_brts.R
@@ -2,10 +2,13 @@
 #'
 #' Read the dataset at specified location and returns branching times for specified tree.
 #'
-#' @param sim character, the name of a simulation model. See \code{get_sim_names()} for possible values.
-#' @param para numeric or character. A four-digits code specifying a set of parameter values. See \code{get_para_values()}
+#' @param sim character, the name of a simulation model. See 
+#' \code{get_sim_names()} for possible values.
+#' @param para numeric or character. A four-digits code specifying a set of 
+#' parameter values. See \code{get_para_values()}
 #' @param mc numeric. The index of the tree to return branching times from.
-#' @param with_extinct logical. \code{with_extinct = F} for the reconstructed tree, \code{with_extinct = T} for the complete tree.
+#' @param with_extinct logical. \code{with_extinct = F} for the reconstructed 
+#' tree, \code{with_extinct = T} for the complete tree.
 #'
 #' @return a numerical vector containing the branching times of the tree.
 #'
