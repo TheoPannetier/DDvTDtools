eb3c1a8ab79b922907ef5c9345626349529e7107
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-05-13 15:20:43 +0200

    fixed argument documentation

--- R/run_from_n.R
+++ R/run_from_n.R
@@ -7,16 +7,11 @@
 #' @param para character or numeric, the code specifying the parameter values used in the simulation.
 #' Call \code{get_para_values()} for possible inputs, and \code{para_to_pars()} for the corresponding parameter values.
 #' @param optim character. The name of the model to fit to the simulated phylogenies. See \code{get_optim_names()} for possible inputs.
-#' @param custom_pars numeric vector, user-specified initial values for the parameters to be optimized.
 #' If \code{NULL} (default), values are initialized using true values used to simulate the trees.
-#' @param outputfile character, the name of the file to save the output data frame in.
-#' Must be specified if \code{save_results = TRUE}.
 #' @param rangemc a numeric vector containing all the indices of the trees to optimize the model on. Default to all the trees in the dataset.
 #' @param methode argument passed to \code{\link[DDD:dd_ML]{dd_ML}} or \code{\link[DDD:dd_ML]{bd_ML}}. See the \pkg{DDD} documentation for more info.
 #' @param optimmethod argument passed to \code{\link[DDD:dd_ML]{dd_ML}} or \code{\link[DDD:dd_ML]{bd_ML}}. See the \pkg{DDD} documentation for more info.
 #' @param tol argument passed to \code{\link[DDD:dd_ML]{dd_ML}} or \code{\link[DDD:dd_ML]{bd_ML}}. See the \pkg{DDD} documentation for more info.
-#' @param save_results logical. Should save the results to \code{outputfile} (default) or not.
-#' @param return_res logical. Should results be returned? Default to \code{FALSE}.
 #' @param jobID is only relevant for metadata. Job scripts pass their ID to this argument. \code{NA} if run locally.
 #' @param num_cycles numeric, passed to \code{dd_ML}/\code{bd_ML}. Number of cycles of optimisation.
 #'
