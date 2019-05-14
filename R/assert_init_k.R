bf621a737a2f2b83428f62894fa51e00e8662b3d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-05-14 12:00:19 +0200

    fixed functions and scripts naming

---
+++ R/assert_init_k.R
@@ -0,0 +1,16 @@
+#' Asserts input init parameter is supported
+#'
+#' @param init_k a character for the initial parameter setting, typically passed
+#' from another function
+#'
+#' @author Theo Pannetier
+#'
+#' @export
+
+assert_init <- function(init_k) {
+  if(!init_k %in% get_possible_init_k()) {
+    stop(
+      "Input init is incorrect. Call get_possible_init_k() for accepted values"
+      )
+  }
+}
---
+++ R/get_possible_init_k.R
@@ -0,0 +1,12 @@
+#' Accepted values of argument init_k
+#'
+#' Returns posssible value of init_k (initialisation value of K for model
+#' optimisation). For \code{true_k} K is initialised at the true value (40),
+#' for \code{from_n} it is initialised at N, the nb  of tips in the tree
+#'
+#' @author Theo Pannetier
+#'
+#' @export
+get_possible_init_k <- function() {
+  c("true_k", "from_n")
+}
