25a375a09e98b38a51378bc07af2bf6cff4c840d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-08-23 14:52:33 +0200

    - Custom parameter values can be provided by the user instead of the default DDvTD ones - Changed a few variables names to make things more readable

--- R/simulate_trees.R
+++ R/simulate_trees.R
@@ -8,6 +8,9 @@
 #' 'TD'.
 #' @param para a 4-digit code used to set the parameter values used for the
 #' simulation. See \code{parameter_list} for possible values.
+#' @param custom_pars a 4-element numeric vector containing c(age, pars)
+#' to supply custom parameters (i.e., values not covered with the \code{para}
+#' option). Either \code{para} or \code{custom_pars} must be supplied, not both.
 #' @param nbmc Integer. How many trees should be generated?
 #' @param outputfile path and name for output file, by default generated
 #' automatically from \code{sim} and \code{para}.
@@ -23,18 +26,28 @@
 #'
 #' @export
 
-simulate_trees <- function(
-  sim,
-  para,
-  nbmc = 1000,
-  outputfile = paste0("./data/sim/sim", sim, "-", para, ".RData"),
-  save_results = TRUE,
-  return_results = TRUE
-  ){
+# simulate_trees(
+#   sim = "TD",
+#   custom_pars = td_ml_pars,
+#   outputfile = "data/setophaga/02_sim/setophaga_simTD_trees.RData",
+#   save_results = TRUE,
+#   return_results = FALSE
+# )
+
+simulate_trees <- function(sim,
+                           para = NULL,
+                           custom_pars = NULL,
+                           nbmc = 1000,
+                           outputfile = paste0(
+                             "./data/sim/sim", sim, "-", para, ".RData"
+                             ),
+                           save_results = TRUE,
+                           return_results = TRUE
+){
+  set.seed(42)
   assert_DDvTD_wd()
   assert_sim(sim)
-  assert_para(para)
-  if(!is.numeric(nbmc)){
+  if(!is.numeric(nbmc)) {
     stop("argument nbmc should be a numeric.")
   }
   if(!is.character(outputfile)){
@@ -47,26 +60,44 @@ simulate_trees <- function(
     stop("argument return_results should be a logical")
   }
 
-  set.seed(42)
+  # Check model parameters input and set initial parameters
+  if (is.null(para)) {
+    if (is.null(custom_pars)) {
+      stop("Either para or custom_pars must be supplied.")
+    } else {
+      if(!is.numeric(custom_pars) | length(custom_pars) != 4) {
+        stop("custom_pars must be a length 4 numeric vector.")
+      } else {
+        age <- custom_pars[1]
+        pars <- custom_pars[2:4]
+      }
+    }
+  } else {
+    if (is.null(custom_pars)) {
+      assert_para(para)
+      age <- DDvTDtools::para_to_pars(para)[1]
+      pars <- DDvTDtools::para_to_pars(para)[2:4]
+    } else {
+      stop("Either para or custom_pars must be supplied, not both.")
+    }
+  }
 
-  pars <- para_to_pars(para)
-  cat("Parameters:", pars, "\n")
+  cat("Input values:", pars, "\n")
   cat(paste("File will be saved at: ", outputfile, "\n"))
 
   trees <- list()
-  for(mc in 1:nbmc)
-  {
+  for (mc in 1:nbmc) {
     cat("Simulating tree ", mc, "...\n")
-    if(sim == "DD"){
-      trees[[mc]] = DDD::dd_sim(pars[2:4], pars[1])
+    if(sim == "DD") {
+      trees[[mc]] = DDD::dd_sim(pars = pars, age = age)
     } else {
-      trees[[mc]] = DDD::td_sim(pars[2:4], pars[1])
+      trees[[mc]] = DDD::td_sim(pars = pars, age = age)
     }
-    if (save_results){
+    if (save_results) {
       save(trees, sim, pars, file = outputfile)
     }
   }
-  if (return_results == TRUE){
+  if (return_results == TRUE) {
     return(trees)
   }
 }
