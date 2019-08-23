74e1ca5c5116c232b1fe2a12057c56f69cfa742a
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-08-23 14:42:19 +0200

    - Fixed variable name

--- R/fit_td_setophaga.R
+++ R/fit_td_setophaga.R
@@ -1,27 +1,27 @@
 #' Fit the time-dependent model on Setophaga phylogeny
-#' 
+#'
 #' Run the maximum likelihood of the time-dependent model on the phylogeny of
 #' Setophaga warblers
-#' 
+#'
 #' @param lambda_ini numeric, the initial value for parameter lambda0
 #' @param mu_ini numeric, the initial value for parameter mu0
 #' @param k_ini numeric, the initial value for parameter K
-#' @param outptufile character. Name of the outputfilen should be a .rds
-#' 
+#' @param outputfile character. Name of the outputfile should be a .rds
+#'
 #' @author Théo Pannetier
 #' @export
 
 fit_td_setophaga <- function(lambda_ini, mu_ini, k_ini, outputfile) {
-  
+
   if (any(!is.numeric(c(lambda_ini, mu_ini, k_ini)))) {
     stop("Input parameters are not numeric.")
   }
   if (!is.character(outputfile)) {
     stop("outputfile must be a character")
   }
-  
+
   load(file = "data/setophaga/branching_times.RData")
-  
+
   out <- DDD::bd_ML(
     brts = brts,
     initparsopt = c(
@@ -35,7 +35,7 @@ fit_td_setophaga <- function(lambda_ini, mu_ini, k_ini, outputfile) {
     methode = "ode45",
     num_cycles = 5
   )
-  
+
   fit <- data.frame(
     lambda_ini = lambda_ini,
     mu_ini = mu_ini,
@@ -45,7 +45,7 @@ fit_td_setophaga <- function(lambda_ini, mu_ini, k_ini, outputfile) {
     mu_ml = out$mu0,
     k_ml = out$lambda1
   )
-  
+
   saveRDS(fit, file = paste0("data/setophaga/", outputfile))
-  
+
 }
