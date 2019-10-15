68f1d6338511695373481dab93310dcf8df8986d
Theo Pannetier <t.s.c.pannetier@rug.nl>
2019-10-15 14:38:12 +0100

    Default now returns only the subset of para values used in the main paper. Included option to call all previous values.

--- R/get_para_values.R
+++ R/get_para_values.R
@@ -2,17 +2,31 @@
 #'
 #' Input values currently accepted for para.
 #'    Use \code{para_to_pars} to translate para into paramter values.
+#' @param incl_unused logical, specify whether you want to include values that
+#' have been considered in earlier phases of the project.
 #'
 #' @author Theo Pannetier
 #' @export
 #' @seealso \code{para_to_pars()}
 
-get_para_values <- function(){
-  c(
-    1211, 1221, 1231, 1241,
-    2211, 2221, 2231, 2241,
-    3211, 3221, 3231, 3241,
-    4211, 4221, 4231, 4241,
-    3411, 3421, 3431, 3441
+get_para_values <- function(incl_unused = FALSE) {
+  if (!is.logical(incl_unused)) {
+    stop("incl_unused must be logical")
+    }
+  if (!incl_unused) {
+    c(
+      1211, 1241,
+      2211, 2241,
+      3211, 3241,
+      4211, 4241
     )
+  } else {
+    c(
+      1211, 1221, 1231, 1241,
+      2211, 2221, 2231, 2241,
+      3211, 3221, 3231, 3241,
+      4211, 4221, 4231, 4241,
+      3411, 3421, 3431, 3441
+    )
+  }
 }
