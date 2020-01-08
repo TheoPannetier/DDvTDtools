87739d9c14e852eddde373a40bbf660584d26222
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-01-08 14:04:41 +0000

    fixed optim filenames
    Naming was unconsistent between the two init_k values:
    - simTD_optimDD-1241.rds (for "true_k")
    - simTD_optimDD_1241_from_n.rds (for "from_n")
    This is now
    - simTD_optimDD_1241_true_k.rds
    - simTD_optimDD_1241_from_n.rds

--- R/read_optim_table.R
+++ R/read_optim_table.R
@@ -22,12 +22,7 @@ read_optim_table <- function(sim, optim, para, init_k = "true_k"){
   assert_optim(optim)
   assert_init_k(init_k)
 
-  if (init_k == "true_k") {
-    res_filename <- paste0("sim", sim, "_optim", optim, "-", para, ".rds")
-  } else if (init_k == "from_n") {
-    res_filename <- paste0("sim", sim, "_optim", optim, "_", para, "_from_n.rds")
-  }
-
+  res_filename <- paste0("sim", sim, "_optim", optim, "_", para, "_", init_k,".rds")
 
   if(!file.exists(paste0("data/optim/", res_filename))){
     stop(paste0("data/optim/",res_filename, " does not exist"))
