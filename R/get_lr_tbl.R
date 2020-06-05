61eb751a9bd685460a88c33cccdfc39058d62f78
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-06-05 01:13:03 +0100

    NAs displayed as warning

--- R/get_lr_tbl.R
+++ R/get_lr_tbl.R
@@ -40,6 +40,10 @@ get_lr_tbl <- function(sim,
       mc,
       "lr" = loglik_DD - loglik_TD
     )
+  nb_NAs <- sum(which(is.na(lr_tbl$lr)))
+  if (nb_NAs > 0) {
+    warning("logLR could not be computed for", nb_NAs, "trees.")
+  }
 
   return(lr_tbl)
 }
