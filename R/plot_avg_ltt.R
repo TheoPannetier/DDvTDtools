a2ea2c111407e5527192aa0f688f8ab5c4d9e4df
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-06-18 15:34:01 +0100

    high K in main text, high lambda in supplementary?

--- R/plot_avg_ltt.R
+++ R/plot_avg_ltt.R
@@ -5,18 +5,40 @@
 #' averaged over 1000 trees with [TreeSim::LTT.plot.gen()].
 #'
 #' @inheritParams params_doc
-#' @param include_ltts logical. If `TRUE`, the LTTs of 500 trees are also
-#' plotted with transparence.
+#' @param trees a vector of trees indices. If supplied, the LTTs of these trees
+#' will also be plotted.
 #'
 #' @author Théo Pannetier
 #' @export
 
-plot_avg_ltt <- function(para, with_extinct = FALSE, include_ltts = TRUE) {
+plot_avg_ltt <- function(para, with_extinct = FALSE, trees = NULL) {
   assert_DDvTD_wd()
   assert_para(para)
   if(!is.logical(with_extinct)) {
     stop('with_extinct must be a logical')
   }
+
+  phylos <- list(
+    "DD" = read_sim_multiPhylo(
+      sim = "DD",
+      para = para,
+      with_extinct = with_extinct
+    ),
+    "TD" = read_sim_multiPhylo(
+      sim = "TD",
+      para = para,
+      with_extinct = with_extinct
+    )
+  )
+  crown_age <- para_to_pars(para)[1]
+  time_seq <- seq(-crown_age, 0, by = 1)
+
+  avg_ltt_tbl <- purrr::map(
+    phylos,
+    function(x) {
+      phylobates::get_summary_ltt(phylos = x, time_seq = time_seq)
+    }
+  ) %>% dplyr::bind_rows(.id = "sim")
 
   # Get ltt tbls for both DD and TD
   ltt_tbls_DD <- get_ltt_tbls(
@@ -35,7 +57,7 @@ plot_avg_ltt <- function(para, with_extinct = FALSE, include_ltts = TRUE) {
     ltt_tbls_DD[[1]], ltt_tbls_TD[[1]]
   )
   # Gather the rest as a single tibble
-  if (include_ltts) {
+  if (!is.null(trees)) {
     ltt_tbls_DD[[1]] <- NULL
     tbl_DD <- lapply(seq_along(ltt_tbls_DD), function(i) {
       ltt_tbls_DD[[i]] %>% dplyr::mutate("mc" = i)
@@ -53,6 +75,30 @@ plot_avg_ltt <- function(para, with_extinct = FALSE, include_ltts = TRUE) {
     ymax <- 100
   }
   # Main plot with average LTTs
+  avg_ltt_tbl %>% ggplot2::ggplot(
+    ggplot2::aes(x = t, y = mean_n, colour = sim)
+    ) +
+    ggplot2::geom_line(size = 1) +
+    ggplot2::scale_colour_manual(values = c("green4", "blue")) +
+    ggplot2::geom_hline(
+      yintercept = DDvTDtools::para_to_pars(para)[4],
+      color = "grey50",
+      linetype = "dashed"
+    ) +
+    ggplot2::coord_cartesian(ylim = c(0, ymax)) +
+    ggplot2::scale_y_continuous(breaks = seq(0, ymax, by = 20)) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(
+      text = ggplot2::element_text(size = 14),
+      axis.title.y = ggplot2::element_text(vjust = 0.5, size = 16),
+      axis.title.x = ggplot2::element_text(size = 16)
+    ) +
+    ggplot2::labs(
+      title = make_plot_title_expr(para),
+      x = "Time",
+      y = "Number of lineages"
+    )
+
   ltt_plot <- ggplot2::ggplot(
     avg_ltt_tbl,
     ggplot2::aes(x = time, y = N, color = sim)
@@ -78,16 +124,16 @@ plot_avg_ltt <- function(para, with_extinct = FALSE, include_ltts = TRUE) {
       y = "Number of lineages"
     )
   # Add all individual LTTs in transparency
-  if (include_ltts) {
+  if (!is.null(trees)) {
     ltt_plot <- ltt_plot +
       ggplot2::geom_step(
         ggplot2::aes(group = mc),
-        data = tbl_DD %>% dplyr::filter(mc >= 500), # not enough room
+        data = tbl_DD %>% dplyr::filter(mc %in% trees), # not enough room
         alpha = 0.01
       ) +
       ggplot2::geom_step(
         ggplot2::aes(group = mc),
-        data = tbl_TD %>% dplyr::filter(mc >= 500),
+        data = tbl_TD %>% dplyr::filter(mc %in% trees),
         alpha = 0.01
       )
   }
