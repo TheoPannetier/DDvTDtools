cb0b4a4159f9257b84915d5814f5c9cea5102364
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-06-24 23:38:15 +0100

    import get_summary_ltt_tbl from phylobates to plot LTTs

---
+++ R/get_summary_ltt_tbl.R
@@ -0,0 +1,10 @@
+#' Compute summary metrics for a set of phylogenetic trees
+#'
+#' See \code{magrittr::\link[phylobates:get_summary_ltt_tbl]{\%>\%}} for details.
+#'
+#' @name get_summary_ltt_tbl
+#' @rdname get_summary_ltt_tbl
+#' @keywords internal
+#' @export
+#' @importFrom phylobates get_summary_ltt_tbl
+NULL
