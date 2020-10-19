c3157d997871cd682d78a2af28db76f944a9f638
Theo Pannetier <t.s.c.pannetier@rug.nl>
2020-10-19 11:47:23 +0200

    remove phylobates dependency

--- R/get_summary_ltt_tbl.R
+++ R/get_summary_ltt_tbl.R
@@ -1,10 +1,47 @@
-#' Compute summary metrics for a set of phylogenetic trees
+#' Get lineage-through-time summaries for a set of phylogenetic trees
 #'
-#' See \code{magrittr::\link[phylobates:get_summary_ltt_tbl]{\%>\%}} for details.
+#' Computes the mean, SD, median, and some quantiles for the number of lineages
+#' through time across a set of trees.
 #'
-#' @name get_summary_ltt_tbl
-#' @rdname get_summary_ltt_tbl
-#' @keywords internal
+#' @param phylos a list of phylogenetic trees.
+#' @param time_seq a numeric vector containing the sequence of time points at
+#' which the summaries are to be computed.
+#'
+#' @author Théo Pannetier
 #' @export
-#' @importFrom phylobates get_summary_ltt_tbl
-NULL
+
+get_summary_ltt_tbl <- function(phylos, time_seq) {
+
+  ltts <- TreeSim::LTT.plot.gen(list(phylos))
+  # drop avg LTT
+  ltts[[1]] <- NULL
+
+  ltt_funcs <- purrr::map(
+    ltts,
+    function(ltt) {
+      ltt_func <- stats::stepfun(
+        x = ltt[-1, 1],
+        y = ltt[, 2]
+      )
+      return(ltt_func)
+    })
+
+  summary_tbl <- tidyr::expand_grid(
+    "i" = seq_along(ltt_funcs),
+    "t" = time_seq
+  ) %>%
+    dplyr::mutate(
+      "n" = purrr::map2_dbl(i, t, function(i, t) ltt_funcs[[i]](t))
+    ) %>%
+    dplyr::group_by(t) %>%
+    dplyr::summarise(
+      "mean_n" = mean(n),
+      "sd_n" = sd(n),
+      "median" = median(n),
+      "q1" = quantile(n, 0.25),
+      "q3" = quantile(n, 0.75),
+      "qt_10" = quantile(n, 0.10),
+      "qt_90" = quantile(n, 0.90)
+    )
+  return(summary_tbl)
+}
